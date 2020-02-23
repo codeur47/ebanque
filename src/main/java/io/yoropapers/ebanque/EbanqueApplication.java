03e0c02f2efe536e8baf890ae38b3667fa0539eb
Yoro Ange Carmel <ange-carmel.yoro@atos.net>
2020-02-23 14:31:46 +0000

    Mise en place environnement

---
+++ src/main/java/io/yoropapers/ebanque/EbanqueApplication.java
@@ -0,0 +1,13 @@
+package io.yoropapers.ebanque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbanqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbanqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/yoropapers/ebanque/EbanqueApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yoropapers.ebanque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbanqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
